572c1e68ec1be2f42a3f5208053707e8a5f12afe
alan runyan <runyaga@gmail.com>
2011-12-29 00:11:45 -0600

    remove colorpicker field, remove link models, remove form examples found in ptah_models. this example has been substantially simplified

--- ptah_simpleauth/ptah_simpleauth/app.py
+++ ptah_simpleauth/ptah_simpleauth/app.py
@@ -9,9 +9,6 @@ import ptah
 # Your custom auth plugin
 from ptah_simpleauth.auth import User
 
-# Import models
-from ptah_simpleauth import models
-
 auth_policy = AuthTktAuthenticationPolicy('secret')
 session_factory = UnencryptedCookieSessionFactoryConfig('secret')
 
@@ -37,9 +34,6 @@ def main(global_config, **settings):
 
     # we love them routes
     config.add_route('root', '/')
-    config.add_route('contact-us', '/contact-us.html')
-    config.add_route('edit-links', '/links/{id}/edit',
-                     factory=models.factory, use_global_views=True)
     config.add_route('login', '/login.html')
     config.add_route('logout', '/logout.html')
 
@@ -65,25 +59,6 @@ def main(global_config, **settings):
         user = User('Admin', 'admin', 'admin@ptahproject.org', '12345')
         Session.add(user)
 
-    # Bootstrap application data with some links; we use SQLAlchemy
-    # directly so there are not application events being fired to apply owner
-
-    links = {'sqlalchemy':'http://www.sqlalchemy.org/',
-             'pyramid':'http://docs.pylonsproject.org/',
-             'enfoldsystems':'http://www.enfoldsystems.com/',
-             'bootstrap':'http://twitter.github.com/bootstrap/',
-             'chameleon':'http://chameleon.repoze.org/',
-             'sqlite':'http://www.sqlite.org/'}
-
-    for name, url in links.items():
-        if not Session.query(models.Link)\
-               .filter(models.Link.href == url).all():
-            link = models.Link(title=name,
-                               href=url,
-                               color='#0000ff')
-            Session.add(link)
-
-    # Need to commit links to database manually.
     transaction.commit()
 
     return config.make_wsgi_app()
